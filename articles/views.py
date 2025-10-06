529c460516d3ad2d8a07e830312380515b52fb05
frankzhang-spqr <sushanth.moole@gmail.com>
2025-10-05 22:34:04 -0500

    Chnaged from Django to Jekyll

--- articles/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    """View function for the home page of the Articles of Confederation site."""
-    return render(request, 'index.html')
--- document_viewer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'document_viewer'
-
-urlpatterns = [
-    path('<str:document_name>/', views.document_gallery, name='document_gallery'),
-]
